14999b84c3eb5a53aefd21a5d6a22d251a6b2b98
madhura-1992 <madhurah1992@gmail.com>
2024-11-11 13:06:34 +0530

    commit1

---
+++ src/test/java/Practice/Sample.java
@@ -0,0 +1,11 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+@Test
+	public void print() {
+		System.out.println("hello");
+	}
+}
---
+++ src/test/java/Practice/Sample1.java
@@ -0,0 +1,11 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class Sample1 {
+
+	@Test
+	public void print() {
+		System.out.println("hello hi");
+	}
+}
